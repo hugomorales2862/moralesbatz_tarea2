7505813057e17496ffade272aa92fe705feae521
Hugo Morales <moralesbatzh@gmail.com>
2023-07-09 12:45:13 -0600

    hice un nuevo formulario para mejorar el ejemplo

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-// OBTENER ELEMENTOS DEL DOM
-// MEDIANTE SU ID
-let titulo = document.getElementById('pagina');
-let boton = document.getElementById('boton')
+ // OBTENER ELEMENTOS DEL DOM MEDIANTE SU ID
+ let titulo = document.getElementById('pagina');
+ let boton = document.getElementById('boton');
 
-// console.log(titulo)
+ // Asigna el valor al atributo 'name'
+ titulo.name = 'miNombre';
 
-// con selectores
-let enlace = document.querySelector('a');
-let formulario = document.querySelector('form')
-let texto = document.querySelectorAll('.text-success')
-let boton2 = document.querySelector('#boton')
-let enlaceFB = document.querySelector('a[href*="facebook"]')
+ // Verifica el nuevo valor asignado
+ console.log("Valor del atributo 'name': " + titulo.name);
 
-let enlacesSeguros = document.querySelectorAll('a[href^="https"]');
+ // OBTENER ELEMENTOS DEL DOM CON SELECTORES
+ let enlace = document.querySelector('a');
+ let formulario = document.querySelector('form');
+ let texto = document.querySelectorAll('.text-success');
+ let boton2 = document.querySelector('#boton');
+ let enlaceFB = document.querySelector('a[href*="facebook"]');
 
-let cajaParrafos = document.getElementById('container')
+ let enlacesSeguros = document.querySelectorAll('a[href^="https"]');
+
+ let cajaParrafos = document.querySelector('.container');
+
+ // CREAR ELEMENTOS DEL DOM
+ const tablaGenerada = document.createElement('table');
+ const parrafoNuevo = document.createElement('p');
+
+ // ASIGNAR VALORES Y PROPIEDADES A LOS ELEMENTOS
+ parrafoNuevo.textContent = "Ejemplo de cómo se crea un párrafo desde JS";
+ parrafoNuevo.classList.add('text-info', 'fw-bold');
+
+ // AGREGAR ELEMENTOS AL DOM
+ cajaParrafos.appendChild(tablaGenerada);
+ cajaParrafos.appendChild(parrafoNuevo);
+
+ // MODIFICAR ATRIBUTOS DE ELEMENTOS EXISTENTES
+ formulario.action = "/controladores/guardar.php";
+ let input = document.querySelector('input');
+
+ console.log(titulo.classList);
+
+ // ASIGNAR VALORES AL ATRIBUTO TEXT
+ titulo.classList.add('fs-6');
+ titulo.classList.remove('text-success');
+ titulo.classList.add('text-danger');
+
+ // ASIGNAR VALORES AL ATRIBUTO HREF
+ let enlaceHermano = document.querySelector('a[href*="google"]');
+ enlaceHermano.href = "https://www.openai.com";
+ enlaceHermano.innerText = "Ir a OpenAI";
+ console.log(enlaceHermano);
+
+ // EVENT LISTENER
+ document.addEventListener('dblclick', (e) => {
+     console.log(e);
+ });
