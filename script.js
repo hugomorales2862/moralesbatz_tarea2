52fe59a4cfb475c0a873e32e8cbd0780f09b8ec8
Hugo Morales <moralesbatzh@gmail.com>
2023-07-09 12:20:03 -0600

    AHORA OBTENEMOS LOS ELEMENTOS MEDIANTE LOS SELECTORES

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 // MEDIANTE SU ID
 let titulo = document.getElementById('pagina');
 let boton = document.getElementById('boton')
+
+// console.log(titulo)
+
+// con selectores
+let enlace = document.querySelector('a');
+let formulario = document.querySelector('form')
+let texto = document.querySelectorAll('.text-success')
+let boton2 = document.querySelector('#boton')
+let enlaceFB = document.querySelector('a[href*="facebook"]')
+
+let enlacesSeguros = document.querySelectorAll('a[href^="https"]');
+
+let cajaParrafos = document.getElementById('container')
