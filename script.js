019c07684dd7c4a46e9327c4b738cf26a30c11f2
Hugo Morales <moralesbatzh@gmail.com>
2023-07-09 13:20:47 -0600

    se crea el archivo js. para el documento html

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
  // OBTENER ELEMENTOS DEL DOM MEDIANTE SU ID
  let titulo = document.getElementById('pagina');
  let boton = document.getElementById('boton');
+ let formulario = document.querySelector('form');
+ let labelHoraSalida = document.getElementById('label_hora_salida');
+ 
+ // MODIFICAR TEXTO DEL LABEL
+ labelHoraSalida.textContent = 'Hora de salida';
 
+ // ASIGNAR VALORES AL FORMULARIO
+ formulario.action = "/moralesb_recuperacion/controladores/visita/guardar.php";
+ formulario.method = "POST";
  // Asigna el valor al atributo 'name'
  titulo.name = 'miNombre';
 
  // Verifica el nuevo valor asignado
  console.log("Valor del atributo 'name': " + titulo.name);
 
- // OBTENER ELEMENTOS DEL DOM CON SELECTORES
- let enlace = document.querySelector('a');
- let formulario = document.querySelector('form');
- let texto = document.querySelectorAll('.text-success');
- let boton2 = document.querySelector('#boton');
- let enlaceFB = document.querySelector('a[href*="facebook"]');
+// console.log(titulo)
 
- let enlacesSeguros = document.querySelectorAll('a[href^="https"]');
+// con selectores
+let enlace = document.querySelector('a');
+let formulario = document.querySelector('form')
+let texto = document.querySelectorAll('.text-success')
+let boton2 = document.querySelector('#boton')
+let enlaceFB = document.querySelector('a[href*="facebook"]')
 
- let cajaParrafos = document.querySelector('.container');
+let enlacesSeguros = document.querySelectorAll('a[href^="https"]');
 
- // CREAR ELEMENTOS DEL DOM
- const tablaGenerada = document.createElement('table');
- const parrafoNuevo = document.createElement('p');
+let cajaParrafos = document.getElementById('container')
 
- // ASIGNAR VALORES Y PROPIEDADES A LOS ELEMENTOS
- parrafoNuevo.textContent = "Ejemplo de cómo se crea un párrafo desde JS";
- parrafoNuevo.classList.add('text-info', 'fw-bold');
+// HIJOS DEL ELEMENTO
+console.log(cajaParrafos.childNodes)
+console.log(cajaParrafos.children[4].innerText = 'cambio desde JS')
 
- // AGREGAR ELEMENTOS AL DOM
- cajaParrafos.appendChild(tablaGenerada);
- cajaParrafos.appendChild(parrafoNuevo);
 
- // MODIFICAR ATRIBUTOS DE ELEMENTOS EXISTENTES
- formulario.action = "/controladores/guardar.php";
- let input = document.querySelector('input');
 
- console.log(titulo.classList);
 
- // ASIGNAR VALORES AL ATRIBUTO TEXT
- titulo.classList.add('fs-6');
- titulo.classList.remove('text-success');
- titulo.classList.add('text-danger');
+formulario.action = "/controladores/guardar.php"
 
- // ASIGNAR VALORES AL ATRIBUTO HREF
- let enlaceHermano = document.querySelector('a[href*="google"]');
- enlaceHermano.href = "https://www.openai.com";
- enlaceHermano.innerText = "Ir a OpenAI";
- console.log(enlaceHermano);
 
- // EVENT LISTENER
- document.addEventListener('dblclick', (e) => {
-     console.log(e);
- });
+let input = document.querySelector('input')
+
+
+
+console.log(titulo.classList)
+
+titulo.classList.add('fs-6')
+titulo.classList.remove('text-success')
+titulo.classList.add('text-danger')
+
+
+const tablaGenerada = document.createElement('table')
+const parrafoNuevo = document.createElement('p')
+
+parrafoNuevo.textContent = "ejemplo de como se crea un parrafo desde JS"
+parrafoNuevo.classList.add('text-info', 'fw-bold')
+
+cajaParrafos.appendChild(tablaGenerada)
+cajaParrafos.appendChild(parrafoNuevo)
+
+console.log(cajaParrafos.firstChild)
+console.log(cajaParrafos.firstElementChild)
+console.log(cajaParrafos.lastChild)
+console.log(cajaParrafos.lastElementChild)
+console.log(cajaParrafos.nextElementSibling)
+console.log(cajaParrafos.nextSibling)
+console.log(cajaParrafos.previousElementSibling.previousElementSibling)
+console.log(cajaParrafos.parentElement)
+
+
+let enlaceHermano = document.getElementById('email').parentElement.parentElement.parentElement.nextElementSibling
+enlaceHermano.href = "https://www.google.com.gt/"
+enlaceHermano.innerText = "Ir a google"
+console.log(enlaceHermano)
+
+document.addEventListener('dblclick', (e)=>{
+    console.log(e)
+})
